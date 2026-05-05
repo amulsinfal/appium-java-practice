e93fa38dde15883f6492ca8b1376693d5739930f
Amul Sinfal <amul.sinfal@gmail.com>
2026-05-06 00:26:28 +0530

    Add InstallApplication.java file.

---
+++ src/test/java/com/solution/applicationManagement/InstallApplication.java
@@ -0,0 +1,53 @@
+package com.solution.applicationManagement;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import org.testng.annotations.Test;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class InstallApplication {
+	
+	@Test
+	public void testInstallApplication() throws MalformedURLException, URISyntaxException,
+	InterruptedException {
+		
+		// Path to the Android application (.apk)
+		String apkPath = System.getProperty("user.dir") + "//src//test//resources//ApiDemos-debug.apk";
+		
+		// Initialize UiAutomator2Options
+		UiAutomator2Options options = new UiAutomator2Options();
+		options.setCapability("appium:app", apkPath);
+		
+		// The Appium Server URL where the Appium Server is running
+		URL url = new URI("http://0.0.0.0:4723/").toURL();
+		
+		// Initialize the AppiumDriver with the server URL and options
+		AppiumDriver driver = new AndroidDriver(url, options);
+		
+		// Retrieve the package name of the app currently in focus.
+		String appPackage = ((AndroidDriver) driver).getCurrentPackage();
+		System.out.println("REMOVING APPLICATION: " + appPackage);
+		
+		// Removing the package
+		((AndroidDriver)driver).removeApp(appPackage);
+		Thread.sleep(5000);
+		
+		// Prints the state of the application.
+		System.out.println("Current App State: " + ((AndroidDriver) driver).queryAppState(appPackage));
+		System.out.println("INSTALLING APPLICATION: " + appPackage);
+		
+		// Installing the package
+		((AndroidDriver)driver).installApp(apkPath);
+		Thread.sleep(5000);
+		
+		// Prints the state of the application.
+		System.out.println("Current App State: " + ((AndroidDriver) driver).queryAppState(appPackage));
+		
+		// Quits the driver
+		driver.quit();
+	}
+}
